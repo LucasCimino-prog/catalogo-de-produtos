30b8aac4101d6140f7d08f5dec7376f7d7af6450
lucas_cimino <lucasciminoalves@gmail.com>
2026-08-16 14:03:05 -0300

    feat: adição do HealthCheck

---
+++ src/main/java/com/lucasprog/catalogo_de_produtos/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.lucasprog.catalogo_de_produtos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public Map<String, String> healthCheck() {
+        return Map.of("status", "UP");
+    }
+}
