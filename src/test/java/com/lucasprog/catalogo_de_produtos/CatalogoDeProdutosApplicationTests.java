aa85ff6167ea203489a282e64f9719fae15a95fe
lucas_cimino <lucasciminoalves@gmail.com>
2026-08-16 13:41:47 -0300

    first commit

---
+++ src/test/java/com/lucasprog/catalogo_de_produtos/CatalogoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasprog.catalogo_de_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
